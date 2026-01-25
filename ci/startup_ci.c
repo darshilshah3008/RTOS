a1eb8ebb43085aab73758b90ab3769008e7e569c
Darshil Shah <darshilshah3008@gmail.com>
2026-01-24 18:11:08 -0600

    CI: fix ResetISR declaration in startup stub

--- ci/startup_ci.c
+++ ci/startup_ci.c
@@ -1,7 +1,8 @@
 #include <stdint.h>
 
-// Forward declaration
+// Forward declarations
 int main(void);
+void ResetISR(void);
 
 // Minimal vector table + Reset handler for CI link proof.
 // Not intended for flashing; just to make GCC link succeed in CI.
@@ -9,15 +10,11 @@ int main(void);
 __attribute__((section(".isr_vector")))
 void (* const g_pfnVectors[])(void) = {
     (void (*)(void)) (0x20008000), // initial stack pointer (dummy)
-    (void (*)(void)) ResetISR      // reset handler
+    ResetISR                     // reset handler
 };
 
 void ResetISR(void)
 {
-    // In real firmware you would init .data/.bss and call SystemInit, etc.
-    // For CI we just call main.
     (void)main();
-
-    // If main returns, loop forever
     while (1) {}
 }
